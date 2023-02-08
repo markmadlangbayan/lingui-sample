8c91fb412c03f59f963412cb7bdbc3eb0a7062b7
Mark Madlangbayan <e95400@bswhealth.org>
2023-02-08 06:18:33 -0600

    Removed metro-react-native-babel-preset

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'module:@lingui/babel-preset-react',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: ['macros'],
 };
