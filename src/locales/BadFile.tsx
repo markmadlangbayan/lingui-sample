9518db68e3589540607ac0ab90d2a4046312d3f7
Mark Madlangbayan <e95400@bswhealth.org>
2023-02-07 19:44:59 -0600

    Sample

---
+++ src/locales/BadFile.tsx
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+interface IProps {}
+interface IState {}
+
+class BswModule<P, S> extends Component<P, S> {}
+
+export default class ProxyManagementWorkflow extends BswModule<
+  IProps,
+  IState
+> {}
